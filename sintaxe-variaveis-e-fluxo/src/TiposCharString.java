cc349c047b915602d4f6399103921a9b68d2288d
Rodney Lucas V. Oliveira <rodneylucas.oliveira@gmail.com>
2022-09-17 11:36:47 -0300

    update

---
+++ sintaxe-variaveis-e-fluxo/src/TiposCharString.java
@@ -0,0 +1,18 @@
+
+public class TiposCharString {
+	public static void main(String[] args) {
+		char letra = 'A';
+		char numero = 66;
+		System.out.println(letra);
+		System.out.println(numero);
+		numero = (char) (numero + 1);
+		System.out.println(numero);
+		
+		
+		
+		String texto = "Alura cursos de tecnologia";
+		System.out.println(texto);
+	
+	}
+
+}
